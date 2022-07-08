f5ce382f7c19ed80648030e522a0ae51df44fc79
vanparfait <nguemechiaparfait@gmail.com>
2022-07-08 22:44:29 +0100

    avec formulaire

---
+++ index.js
@@ -0,0 +1,17 @@
+const container = document.getElementById("text");
+const text = "Bonjour le Monde";
+const limit = text.length;
+let index = 0;
+
+const idInterval = setInterval(() => {
+  if (index <= limit) {
+    container.textContent = text.slice(0, index++);
+  } else {
+    clearInterval(idInterval);
+  }
+}, 1000);
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  idInterval;
+});
